eba3e557f97db5c30692e08cfc21a81e83fa577f
Your Name <your.email@example.com>
2025-03-31 09:19:32 +0530

    Update Vercel configuration and build script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+set -e
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
